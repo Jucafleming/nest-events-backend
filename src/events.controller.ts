f36c7e52151e79330a5edfaca64d579e736c8221
Joao <jucafleming70@gmail.com>
2024-03-21 11:09:52 -0300

    add Rout Parameters

---
+++ src/events.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Patch, Get, Post, Delete, Param } from "@nestjs/common";
+
+
+
+@Controller('/events')
+export class EventsController{
+
+    @Get()
+    findAll(){}
+    
+    @Get(':id')
+    findOne(@Param('id')id){
+        return id;
+    }
+        
+    @Post()
+    create(){}
+
+    @Patch(':id')
+    update(@Param('id')id){
+        return id;
+    }
+
+    @Delete(':id')
+    remove(@Param('id')id){
+        return id;
+    }
+
+
+}
