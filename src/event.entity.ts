eb7d29c1f29a631144b0974d80102941bea1dea4
Joao <jucafleming70@gmail.com>
2024-03-25 18:19:38 -0300

    add docker and mysql connect

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,8 +1,20 @@
-export class Event{
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
+export class Event{
+    
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @Column()
     when: Date;
+    
+    @Column()
     address: string;
 }
